42890f2a0a5a0c0fdfd654dea146540efd75d9d7
Juan Sebastian Montaño Molina <juxemburg@outlook.com>
2017-09-22 09:37:48 -0500

    se agregaron las plantillas para el wizard, con el fin de facilitar el proceso de la prematrícula.

--- src/app/matricula/matricula.module.ts
+++ src/app/matricula/matricula.module.ts
@@ -2,14 +2,23 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RouterModule } from '@angular/router';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MatriculaWizardComponent } from './matricula-wizard/matricula-wizard.component';
+import { DashboardNavbarComponent } from './dashboard-navbar/dashboard-navbar.component';
 
 @NgModule({
   imports: [
     SharedModule,
     RouterModule.forChild([
-      {path: 'dashboard', component: DashboardComponent}
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+        children: [
+          {path:'', component: MatriculaWizardComponent},
+          {path:'wizard', component: MatriculaWizardComponent}]
+      },
     ])
   ],
-  declarations: [DashboardComponent]
+  declarations: [DashboardComponent, SidebarComponent, MatriculaWizardComponent, DashboardNavbarComponent]
 })
 export class MatriculaModule { }
